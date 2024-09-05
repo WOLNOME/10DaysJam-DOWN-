54f9350697a96eeb925a5741efb8c5a875ccb104
miyanao <k023g0050@g.neec.ac.jp>
2024-09-05 15:12:02 +0900

    Playerクラス基盤の作成

---
+++ DirectXGame/Script/Player.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <Input.h>
+#include "Model.h"
+#include "TextureManager.h"
+#include "WorldTransform.h"
+#include "MyTools.h"
+
+class Player {
+public:
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="model">モデル</param>
+	/// <param name="textureHandle">テクスチャハンドル</param>
+	void Initialize(Model* model, uint32_t textureHandle);
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 移動処理
+	/// </summary>
+	void Translation();
+
+	/// <summary>
+	/// 旋回処理
+	/// </summary>
+	void Rotate();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	/// <param name="viewProjection">ビュープロジェクション</param>
+	void Draw(ViewProjection& viewProjection);
+
+private:
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+	// モデル
+	Model* model_ = nullptr;
+	// テクスチャハンドル
+	uint32_t textureHandle_;
+	// キーボード入力
+	Input* input_ = nullptr;
+
+	// 自キャラの速さ
+	float kCharacterSpeed = 0.2f;
+
+};
