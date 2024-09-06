6db28ca6035612b8d01fe94d2b1100f428d28274
WOLNOME <143981493+WOLNOME@users.noreply.github.com>
2024-09-07 00:23:54 +0900

    フェーズ切り替え着地処理追加

--- DirectXGame/Wall.h
+++ DirectXGame/Wall.h
@@ -21,14 +21,32 @@ private://変数
 	//ワールドトランスフォーム
 	WorldTransform worldTransformWall_[2];
 	WorldTransform worldTransformBlack_;
+	WorldTransform worldTransformPhaseGround_;
+
 	//モデル
 	std::unique_ptr<Model> modelWall_[2];
 	std::unique_ptr<Model> modelBlack_ = nullptr;
+	std::unique_ptr<Model> modelPhaseGround_ = nullptr;
+
 
 private:
 	//壁の長さ
 	const float kWallLength_ = 3200.0f;
 	//落下速度
-	Vector3 fallVelocity_; 
+	Vector3 fallVelocity_;
+
+
+private://フェーズ処理変数
+	//各フェーズの時間
+	const int kPhaseTime_ = 60 * 10;
+	//フェーズタイマー
+	int phaseTimer_ = 0;
+	//プレイヤーの身長(カメラと地面の距離)
+	const float kPlayerHeight_ = 50.0f;
+	//休憩ゾーンが来るフラグ
+	bool isComingPhaseGround_ = false;
+	//着地したフラグ
+	bool isLanding_ = false;
+	
 
 };
