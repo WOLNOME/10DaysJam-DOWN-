057bafc2ebfa558260a00f31d4bba067134edcb2
WOLNOME <143981493+WOLNOME@users.noreply.github.com>
2024-09-13 18:16:49 +0900

    a

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -21,7 +21,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"10DaysJam<DOWN>");
+	win->CreateGameWindow(L"ネオン深淵");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
